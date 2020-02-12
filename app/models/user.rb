16d6a44e572e2ab90a4b06bfc899763f1e091c9d
Isaac Avilez <isaac1180@aol.com>
2020-02-12 09:58:08 -0500

    added a user page with errors if discrepancies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :subscriptions
     has_many :gyms, through: :subscriptions
+
+    validates :name, presence: true
+    validates :name, uniqueness: true 
+
+    has_secure_password
 end
