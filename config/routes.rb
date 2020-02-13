05176634a0e677d8f345ada2a74164dbb813c971
Gregory Diaz <gregdiaz8624@yahoo.com>
2020-02-13 11:58:08 -0500

    almost broke it

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :subscriptions, only: [:new, :create, :destroy]
   resources :gyms, only: [:index, :show, :destroy]
   resources :users, only: [:index, :new, :welcome, :show, :create, :destroy]
+  
+  resources :login, only: [:new, :create]
+  delete "logout", to: "login#destroy", as: "log_out"
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
