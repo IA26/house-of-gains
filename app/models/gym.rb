00a2a40ab4d18d263842a65f7c6be5114b8712e0
Isaac Avilez <isaac1180@aol.com>
2020-02-13 10:40:27 -0500

    added uniqueness for subscriptions / css

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,6 +1,4 @@
 class Gym < ApplicationRecord
     has_many :subscriptions, dependent: :destroy
     has_many :users, through: :subscriptions
-
-
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,6 @@
 class Subscription < ApplicationRecord
     belongs_to :user
     belongs_to :gym
+
+    validates_uniqueness_of :user_id, :scope => :gym_id
 end
